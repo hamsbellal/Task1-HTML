93050a71b710c28c4ac2c96ac5173584101432ed
Hams Saeed <hamssaeed5@gmail.com>
2021-04-01 14:34:14 +0200

    first commit

---
+++ Shared.js
@@ -0,0 +1,22 @@
+document.write('\
+\<fieldset>\
+\<center>\
+\<a href="Index.html"> Home\
+</a> | \
+\<a href="About.html#info">About Us\
+\</a> | \
+\<a href="Contact.html#form">Contact Us\
+\</a> | \
+<a href="Registration.html">Registration\
+\</a> | \
+<a href="Gallary.html">Gallary\
+\</a> | \
+<a href="Article.html">Article\
+\</a> | \
+<a href="CV.html">CV\
+\</a> | \
+\
+</center>\
+\</fieldset>\
+\
+');
